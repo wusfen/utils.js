eae019fe78e27bb24309de566f135430191557cc
wusfen <404315887@qq.com>
2019-05-10 17:27:19 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -10,6 +10,32 @@ var utils = {
       }
     }
     return _obj
+  },
+  toUrlParams: function (value){
+    var params = ''
+
+    function loopValue(parentKey, value){
+      if(value && (value.constructor==Object || value instanceof Array)){
+        for(var k in value){
+          if (!value.hasOwnProperty(k)) continue
+          var key = !parentKey? k: '[' + (isNaN(k)?k:'') + ']'
+          key = parentKey + key
+          loopValue(key, value[k])
+        }
+      } else {
+        if(value !== undefined){
+          if(value === null) value = ''
+          if(typeof value ==='function') value = value()
+          var kv = encodeURIComponent(parentKey).replace(/%20/g, '+')	+ '='
+            + encodeURIComponent(value).replace(/%20/g, '+')
+          //kv = parentKey + '=' + value
+          params += (params? '&': '') + kv
+        }
+      }
+    }
+    loopValue('', value)
+
+    return params
   }
 }
 
